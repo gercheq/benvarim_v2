5e0cd69c68f5406a73a61f4a8b95cc534bcd9dce
yigit <yboyar@gmail.com>
2011-01-04 22:49:31 -0800

    logic to load partial javascripts at the end of the page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
     return "" unless (act.nil? || act.to_s == controller.action_name)
     "selected"
   end
+
+  def add_page_specific_script script_name
+    unless @page_spicific_scripts
+      @page_spicific_scripts = Array.new
+    end
+    @page_spicific_scripts.push script_name
+  end
+  def get_page_specific_scripts
+    return @page_spicific_scripts || Array.new
+  end
 end
