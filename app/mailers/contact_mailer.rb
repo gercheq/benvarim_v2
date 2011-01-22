2d63bf43166d37258105805b2a22f23112b8828f
yigit <yboyar@gmail.com>
2011-01-21 23:42:30 -0800

    trying to run base example

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,9 @@
-class ContactMailer < ActionMailer::Base
-  default :from => "iletisim@benvarim.com"
-  def contact_benvarim
-    recipients "yigit@benvarim.com"
-    from       "olala"
+class Contact < ActionMailer::Base
+  def signup_notification(user)
+    recipients "#{user.name} <yboyar@gmail.com>"
+    from       "My Forum "
     subject    "Please activate your new account"
     sent_on    Time.zone.now
-    body       {}
+    body       { :user => user}
   end
 end
