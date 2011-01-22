da91425b1ab6b73854aa6da131f6280afc49900f
yigit <yboyar@gmail.com>
2011-01-21 23:27:14 -0800

    fixed typo

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,10 +2,9 @@ class ContactMailer < ActionMailer::Base
   default :from => "iletisim@benvarim.com"
   def contact_benvarim
     recipients "yigit@benvarim.com"
-        from       "My Forum "
-        subject    "Please activate your new account"
-        sent_on    Time.zone.now
-        body       { }
-      end
+    from       "team@benvarim.com"
+    subject    "Please activate your new account"
+    sent_on    Time.zone.now
+    body       {}
   end
 end
