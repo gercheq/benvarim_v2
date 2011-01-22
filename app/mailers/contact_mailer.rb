34dcc6ff6ef4404c3c58ab4140af4a16c028c25a
yigit <yboyar@gmail.com>
2011-01-21 23:39:14 -0800

    more tests

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
   default :from => "iletisim@benvarim.com"
   def contact_benvarim
     recipients "yigit@benvarim.com"
-    from       "team@benvarim.com"
+    from       "olala"
     subject    "Please activate your new account"
     sent_on    Time.zone.now
     body       {}
