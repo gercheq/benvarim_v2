82871fe5753c14ee0e00b742351f15821e8687c8
yigit <yboyar@gmail.com>
2011-01-21 23:53:41 -0800

    jet

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,8 +1,6 @@
-class Contact < ActionMailer::Base
-  def signup_notification(user)
-    recipients "#{user.name} <yboyar@gmail.com>"
-    from       "My Forum "
-    subject    "Please activate your new account"
-    sent_on    Time.zone.now
+class ContactMailer < ActionMailer::Base
+  def test
+    mail(:to => "yigit@benvarim.com",
+         :subject => "Jetkinlik'e hoalal!")
   end
 end
