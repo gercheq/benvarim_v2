13e1ca169d8f9a728ae29455897c0a2a208ea5d7
gercheq <gercekk@gmail.com>
2011-02-27 23:41:25 -0800

    new homepage implemented.

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,76 +1,66 @@
 /*
-** benvarim.com user interactions 
+** benvarim.com user interactions
 ** author: gercek karakus - 2011
 **
 */
 
-function fadeInOrder(elem) { 
-  elem.fadeIn(500, function() { 
-    
+function fadeInOrder(elem) {
+  elem.fadeIn(500, function() {
+
     if( $(this).next().length > 0){
-      // fadeIn() next element if exists 
-      fadeInOrder( $(this).next().delay(1000) ); 
+      // fadeIn() next element if exists
+      fadeInOrder( $(this).next().delay(1000) );
     }
     else{
       // display the campaign form
       $('.campaign-form-container').delay(300).animate({ "opacity": ".7" }, 300).animate({ "opacity": "1" }, 300);
-        
+
     }
-    
-  }); 
-}                            
+
+  });
+}
 
 
 /*
-** DOM READY 
+** DOM READY
 */
 $(document).ready(function(){
 
+  fadeInOrder( $("#step-1") );
+
 
   //
-  // Featured Animation
-  //
-  $('.steps-loading').fadeOut(500, function(){
-    $('.steps').fadeIn('fast',function(){
-      
-      //var $tmp = $('#step-1');
-      fadeInOrder( $("#step-1") );
-    });
-  });
-  
-  
-  //
-  // Search Focus & Blur 
+  // Search Focus & Blur
   //
   var searchDefaultText = $(".clear-on-focus").attr("value");
-  
-  $(".clear-on-focus").focus(function(){  
-    if($(this).attr("value") == searchDefaultText) $(this).attr("value", "");  
-  });  
-  $(".clear-on-focus").blur(function(){  
-     if($(this).attr("value") == "") $(this).attr("value", searchDefaultText);  
-  });   
-  
+
+  $(".clear-on-focus").focus(function(){
+    if($(this).attr("value") == searchDefaultText) $(this).attr("value", "");
+  });
+  $(".clear-on-focus").blur(function(){
+     if($(this).attr("value") == "") $(this).attr("value", searchDefaultText);
+  });
+
   //
   // Form Custom Styles
   //
-  $("select, :radio, :checkbox").uniform();  
-  
-  
+  $("select, :radio, :checkbox").uniform();
+
+
   //
   // Clear margin-bottom for .fullcontent-right p(last)
   //
   $('.fullcontent-right p').last().css('margin','0');
-  
-  
+
+
   $('.row').hover(  function(){ $(this).addClass('row-hover'); },
                     function(){ $(this).removeClass('row-hover'); });
-                    
-                    
+
+
   $('.comment-bubble').append('<div class="comment-arrow"></div>');
-  
+
   $('.progress-bar').progressbar({ value: 37 });
-  
+
 });
 
 
