bce0db7dbb5ac0beb535aad7d937ca171e8bb3c8
yigit <yboyar@gmail.com>
2011-03-06 19:42:19 -0800

    heroku compatible translient

--- app/models/page.rb
+++ app/models/page.rb
@@ -83,23 +83,6 @@ class Page < ActiveRecord::Base
       # Based on permalink_fu by Rick Olsen
 
       # Escape str by transliterating to UTF-8 with Iconv
-      s = Iconv.iconv('ascii//ignore//translit', 'utf-8', str).to_s
-
-      # Downcase string
-      s.downcase!
-
-      # Remove apostrophes so isn't changes to isnt
-      s.gsub!(/'/, '')
-
-      # Replace any non-letter or non-number character with a space
-      s.gsub!(/[^A-Za-z0-9]+/, ' ')
-
-      # Remove spaces from beginning and end of string
-      s.strip!
-
-      # Replace groups of spaces with single hyphen
-      s.gsub!(/\ +/, '-')
-
-      return s
+      return str.downcase.gsub(/[^a-z0-9]+/i, '-')
     end
 end
