0bbad2720465894de4bd3c063af272102488d81e
yigit <yboyar@gmail.com>
2011-03-06 18:51:19 -0800

    more tries for heroku, removed default

--- db/migrate/20101204074142_data_to_organization.rb
+++ db/migrate/20101204074142_data_to_organization.rb
@@ -3,8 +3,8 @@ class DataToOrganization < ActiveRecord::Migration
     add_column :organizations, :name, :string
     add_column :organizations, :address, :string
     add_column :organizations, :description, :text
-    add_column :organizations, :approved, :boolean, :default => 'false'
-    add_column :organizations, :active, :boolean, :default => 'false'
+    add_column :organizations, :approved, :boolean
+    add_column :organizations, :active, :boolean
   end
 
   def self.down
