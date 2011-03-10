9f9251aec0dadb485b09d632a52d0777ee34f3bb
yigit <yboyar@gmail.com>
2011-03-09 23:44:39 -0800

    kontaklar iletisime gitsin

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,7 +1,7 @@
 class ContactMailer < ActionMailer::Base
   default :from => "team@benvarim.com"
   def contact_benvarim contact_form
-    recipients "yigit@benvarim.com"
+    recipients "iletisim@benvarim.com"
     from       "iletisim@benvarim.com"
     subject    "Benvarim.com yeni iletisim formu"
     sent_on    Time.zone.now
