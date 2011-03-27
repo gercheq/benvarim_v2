e7105aea334ba5d89c152666566d65d3c010b478
yigit <yboyar@gmail.com>
2011-03-27 15:31:15 -0700

    anasayfada top projeler ve sayfalar. bence biraz kotu durdu, biraz tasarim gerekli (organizasyon sayfasindakinin aynisini koydum, degistirirken etkilenbilir dikkat) re #106

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
     @available_organizations = Organization.available_organizations_simple
+    @top_pages = Page.where("pages.collected > 0").order("pages.collected DESC").limit(3)
+    @top_projects = Project.order("collected DESC").limit(3)
   end
 
   def about
