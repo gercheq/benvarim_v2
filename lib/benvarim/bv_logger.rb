14e053746b626e4b15eaf24a545cd5f020d29494
yigit <yboyar@gmail.com>
2011-04-14 00:20:32 -0700

    paypal encoding depends on account owner's  settings so we have to convert it on our end :/

--- lib/benvarim/bv_logger.rb
+++ lib/benvarim/bv_logger.rb
@@ -1,8 +1,11 @@
 class BvLogger
+  def self.to_my_utf8 seq
+      ::Iconv.conv('UTF-8//IGNORE', 'UTF-8', seq + ' ')[0..-2]
+    end
   def self.log(namespace, content)
     log = Bvlog::create({
-      "namespace" => namespace,
-      "content" => content
+      "namespace" => self.to_my_utf8(namespace),
+      "content" => self.to_my_utf8(content)
       #todo, grab controller and action from current controller
     })
     log.save
