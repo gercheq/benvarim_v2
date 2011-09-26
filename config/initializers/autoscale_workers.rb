535c124ff73ceccd42587dcbc4d369a7150ee255
yigit <yboyar@gmail.com>
2011-09-26 01:25:28 -0500

    disable bug

--- config/initializers/autoscale_workers.rb
+++ config/initializers/autoscale_workers.rb
@@ -1,6 +1,6 @@
 Delayed::Job.auto_scale = true
 if RAILS_ENV == 'production'
-   Delayed::Job.auto_scale_manager = :heroku
+#   Delayed::Job.auto_scale_manager = :heroku
 else
-   Delayed::Job.auto_scale_manager = :local
+#   Delayed::Job.auto_scale_manager = :local
 end
