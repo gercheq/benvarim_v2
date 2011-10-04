7e9f2c7824e65d03eb49fd7d0c34d9c25f4710d6
yigit <yboyar@gmail.com>
2011-10-03 21:47:58 -0500

    new parser for new rails version

--- script/benvarim/export_emails.rb
+++ script/benvarim/export_emails.rb
@@ -3,4 +3,5 @@ emails += User.find(:all, :select => "email").collect do |x| x.email end
 emails += Organization.find(:all, :select => "contact_email").collect do |x| x.contact_email end
 emails += TmpPayment.find(:all, :select => "email").collect do |x| x.email end
 emails += ContactForm.find(:all, :select => "email").collect do |x| x.email end
-emails += Bvlog.where("content like '%payer_email%'").collect {|x| (ActiveSupport::JSON.decode x.content)["payer_email"].sub("\n","")}
+emails += Bvlog.where("content like '%payer_email%'").select("SUBSTRING(content FROM 'payer_email\"\:\"([A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})') as content").limit(5).collect do |x| x.content end
+emails  = emails.uniq
