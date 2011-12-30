056d239dddb4f65c717e9e51ac9df4627296bf68
yigit <yboyar@gmail.com>
2011-12-30 03:56:24 -0800

    goal could not be reached email

--- app/mailers/payment_mailer.rb
+++ app/mailers/payment_mailer.rb
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 class PaymentMailer < ActionMailer::Base
+  # add_template_helper("DateHelper")
   default :from => "iletisim@benvarim.com"
   def thanks payment
     @payment = payment
@@ -26,6 +27,31 @@ class PaymentMailer < ActionMailer::Base
     mail(:to => tmp_payment.email,
          :subject => "Yarım kalan bağışınız. - Benvarim.com",
          "X-SMTPAPI" => '{"category": "payment_incomplete"}')
+  end
+
+
+  def send_goal_not_reached_email date
+    payments = Payment.joins(:page).where("pages.goal > pages.collected AND payments.created_at BETWEEN ? AND ?", date, date + 1.day)
+    payments.each do |payment|
+      if payment.page.can_be_donated?
+        Delayed::Job.enqueue MailJob.new("PaymentMailer", "page_could_not_reach_goal", payment)
+      end
+    end
+  end
+
+  def page_could_not_reach_goal payment
+    @page = payment.page
+    @payment = payment
+    mail(:to => "yboyar@gmail.com",
+         :subject => "#{@page.title} isimli sayfanın son durumu - Benvarim.com",
+         "X-SMTPAPI" => '{"category": "payment_goal_not_reached"}')
+  end
+
+
+
+  def send_5_days_goal_not_reached_email
+    now = Time.now.in_time_zone("Istanbul")
+    PaymentMailer.send_goal_not_reached_email(now - 5.day)
   end
 
 end
