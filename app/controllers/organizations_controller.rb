4c09f0c8fe973129b425a454170754d28c884507
Gercek Karakus <gercekk@gmail.com>
2012-01-22 16:14:23 -0800

    removes error messages from donation and project pages and fixes layout bug if the pages are not active

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -10,9 +10,11 @@ class OrganizationsController < ApplicationController
     @organization = Organization.find(params[:id])
     @top_pages = @organization.pages.where("pages.collected > 0").order("pages.collected DESC").limit(3)
     @projects = @organization.projects
-    if !@organization.can_be_donated?
-      flash.now[:error] = @organization.cant_be_donated_reason
-    end
+    # DISABLING ERROR MESSAGES ON ORGANIZATION PAGES FOR NOW
+    # MIGHT BE RENABLED IN THE FUTURE
+    # if !@organization.can_be_donated?
+    #   flash.now[:error] = @organization.cant_be_donated_reason
+    # end
   end
 
   def new
