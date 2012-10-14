067fd07c7cef945668679472cbf43da073de7ca5
yigit <yboyar@gmail.com>
2012-10-14 13:46:50 -0700

    async re-indexing

--- lib/benvarim/bv_index_rebuilder.rb
+++ lib/benvarim/bv_index_rebuilder.rb
@@ -1,6 +1,7 @@
 class BvIndexRebuilder
   DEFAULT_OPTIONS = {
-    "recreate_index" => false
+    "recreate_index" => false,
+    "sync" => false
   }
   def self.re_index options = {}
     options = DEFAULT_OPTIONS.merge options
@@ -12,16 +13,28 @@ class BvIndexRebuilder
     #re-index all organizations
     puts "will re-index all organizations"
     Organization.all.each do |o|
-      o.re_index
+      if options["sync"]
+        o.re_index
+      else
+        o.index_after_save
+      end
     end
     puts "will re-index all projects"
     Project.all.each do |p|
-      p.re_index
+      if options["sync"]
+        p.re_index
+      else
+        p.index_after_save
+      end
     end
 
     puts "will re-index all pages"
     Page.all.each do |p|
-      p.re_index
+      if options["sync"]
+        p.re_index
+      else
+        p.index_after_save
+      end
     end
     puts "done"
   end
