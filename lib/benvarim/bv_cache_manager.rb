906c158920475868eddab7e1e76af895cdd6cdc7
Berkan Kisaoglu <berkan.kisaoglu@gmail.com>
2012-11-11 17:07:29 -0800

    add cache

---
+++ lib/benvarim/bv_cache_manager.rb
@@ -0,0 +1,10 @@
+class BvCacheManager
+  def self.get_cache_key_for_home_page_stats
+    return "home_page_stats_cache_key_";
+  end
+  
+  def self.get_cache_key_for_home_page_feed
+    return "home_page_feed_cache_key_";
+  end
+  
+end
